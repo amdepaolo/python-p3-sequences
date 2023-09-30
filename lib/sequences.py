16064d29bbac969b6636f12898a0a99c31d0273d
Adam DePaolo <amdepaolo@gmail.com>
2023-09-29 22:36:18 -0400

    passes tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = [0,1]
+    if length == 0:
+        print([])
+    elif length < 3:
+        print(fib_list[0:length])
+    else:
+        for n in range(length - 2):
+            next_num = fib_list[-2] + fib_list[-1]
+            fib_list.append(next_num)
+        print (fib_list)
